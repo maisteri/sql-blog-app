8d91ef47a2e9b304ac99eea3880ca4a4e748ce95
Atte Kivistö <atte.t.kivisto@gmail.com>
2023-09-12 09:58:30 +0300

    up to 13.18 done

---
+++ migrations/20230912_00_initialize_blogs_and_users.js
@@ -0,0 +1,77 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('blogs', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      author: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      url: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    })
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      username: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password_hash: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    })
+    await queryInterface.addColumn('blogs', 'user_id', {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+    })
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('blogs')
+    await queryInterface.dropTable('users')
+  },
+}
